50f3af9eedd225d6d5ef80f8c6dde293a723ac0a
5g4s <kmvz9y6@gmail.com>
2023-10-20 01:52:35 +0900

    Add solution 36

--- src/bin/036.rs
+++ src/bin/036.rs
@@ -1,3 +1,19 @@
+#![allow(non_snake_case)]
+
+use proconio::input;
+use std::f64::consts::PI;
+
 fn main() {
-    todo!();
+    input! {
+        A: f64,
+        B: f64,
+        H: f64,
+        M: f64
+    }
+
+    let short = ((H / 12.0) + (M / (60.0 * 12.0))) * 2.0 * PI;
+    let long = (M / 60.0) * 2.0 * PI;
+    let theta = short - long;
+    let answer = A.powf(2.0) + B.powf(2.0) - 2.0 * A * B * theta.cos();
+    println!("{}", answer.sqrt());
 }
