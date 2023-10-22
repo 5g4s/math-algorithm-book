f0da43447de07f4c12b8670ad670bff0219fc207
5g4s <kmvz9y6@gmail.com>
2023-10-22 16:34:33 +0900

    Add solution 39

--- src/bin/039.rs
+++ src/bin/039.rs
@@ -1,3 +1,27 @@
+#![allow(non_snake_case)]
+
+use std::cmp::Ordering;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        Q: usize,
+        LRX: [(usize, usize, i64); Q]
+    }
+
+    let mut diff = vec![0; N + 1];
+
+    for (L, R, X) in LRX {
+        diff[L - 1] += X;
+        diff[R] -= X;
+    }
+    for i in &diff[1..N] {
+        match i.cmp(&0) {
+            Ordering::Less => print!(">"),
+            Ordering::Equal => print!("="),
+            Ordering::Greater => print!("<"),
+        }
+    }
 }
