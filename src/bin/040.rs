7a3d98957ed8b579b69d08efb8f442a9f8f31edb
5g4s <kmvz9y6@gmail.com>
2023-10-23 23:09:25 +0900

    Add solution 40

--- src/bin/040.rs
+++ src/bin/040.rs
@@ -1,3 +1,24 @@
+#![allow(non_snake_case)]
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        A: [i64; N-1],
+        M: usize,
+        B: [usize; M]
+    }
+
+    let mut accumulation = vec![0; N + 1];
+
+    for i in 0..N - 1 {
+        accumulation[i + 1] = accumulation[i] + A[i];
+    }
+
+    let mut ans = 0;
+    for j in 0..M-1 {
+        ans += (accumulation[B[j + 1] - 1] - accumulation[B[j] - 1]).abs();
+    }
+    println!("{}", ans);
 }
