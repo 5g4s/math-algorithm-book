701be309696cae5a084087746657ca4254abbc12
5g4s <kmvz9y6@gmail.com>
2023-10-25 00:42:48 +0900

    Add solution 41

--- src/bin/041.rs
+++ src/bin/041.rs
@@ -1,3 +1,25 @@
+#![allow(non_snake_case)]
+
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        T: usize,
+        N: usize,
+        LR: [(usize, usize); N]
+    }
+
+    let mut accumulation = vec![0; T + 1];
+
+    for (L, R) in LR {
+        accumulation[L] += 1;
+        accumulation[R] += -1;
+    }
+
+    let mut ans = 0;
+    for j in &accumulation[0..T] {
+        ans += j;
+        println!("{}", ans);
+    }
 }
