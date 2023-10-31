b53b2c3b822acbbcfead9778159da7f091394c9b
5g4s <kmvz9y6@gmail.com>
2023-10-31 23:25:47 +0900

    Add solution 43

--- src/bin/043.rs
+++ src/bin/043.rs
@@ -1,3 +1,24 @@
+#![allow(non_snake_case)]
+
+use ac_library::Dsu;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        M: usize,
+        AB: [(usize, usize); M]
+    }
+
+    let mut ds = Dsu::new(N + 1);
+
+    for (A, B) in AB {
+        ds.merge(A, B);
+    }
+
+    if ds.size(1) == N {
+        print!("The graph is connected.");
+    } else {
+        print!("The graph is not connected.");
+    }
 }
