41b3aeb67a566158c45496925922b5381022b187
5g4s <kmvz9y6@gmail.com>
2023-10-31 23:25:22 +0900

    Add solution 42

--- src/bin/042.rs
+++ src/bin/042.rs
@@ -1,3 +1,16 @@
+#![allow(non_snake_case)]
+
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+    }
+
+    let mut ans = 0;
+    for i in 1..=N {
+        ans += i * (N / i) * (1 + (N / i)) / 2;
+    }
+    println!("{}", ans);
 }
