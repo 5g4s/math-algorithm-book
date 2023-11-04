6786cecb8a147f3b493e3827365ee23df23ebecf
5g4s <kmvz9y6@gmail.com>
2023-11-05 00:37:18 +0900

    Add solution 45

--- src/bin/045.rs
+++ src/bin/045.rs
@@ -1,3 +1,29 @@
+#![allow(non_snake_case)]
+
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        M: usize,
+        ab: [(usize, usize); M]
+    }
+
+    let mut count = vec![0; N];
+    for (a, b) in ab {
+        if a < b {
+            count[b-1] += 1;
+        } else {
+            count[a-1] += 1;
+        }
+    }
+
+    let mut answer = 0;
+    for i in count {
+        if i == 1 {
+            answer += 1;
+        }
+    }
+    println!("{}", answer);
 }
