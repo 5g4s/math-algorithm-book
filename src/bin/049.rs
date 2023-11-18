ce3448b5b9efb1c4e15cedebd908de944ec6caa8
5g4s <kmvz9y6@gmail.com>
2023-11-18 22:48:25 +0900

    Add solution 49

--- src/bin/049.rs
+++ src/bin/049.rs
@@ -1,3 +1,22 @@
+#![allow(non_snake_case)]
+const MOD: usize = 1_000_000_007;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        N: usize
+    }
+
+    let mut a = [1, 1];
+
+    for _ in 2..N {
+        let mut ans = a[1] + a[0];
+        if ans >= MOD {
+            ans -= MOD;
+        };
+        a[0] = a[1];
+        a[1] = ans;
+    }
+    println!("{}", a[1]);
 }
