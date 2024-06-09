e15148325318b8dac5f7f733e19b73c6e251d9b1
5g4s <kmvz9y6@gmail.com>
2024-06-09 19:19:00 +0900

    Add Solution 9

--- src/bin/009.rs
+++ src/bin/009.rs
@@ -1,3 +1,28 @@
+#![allow(non_snake_case)]
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        N: usize,
+        S: usize,
+        A: [usize; N]
+    }
+
+    let mut dp = vec![false; S+1];
+    dp[0] = true;
+
+    for &a in &A{
+        for i in (0..=S).rev(){
+            if dp[i] && i + a <= S {
+                dp[i + a] = true;
+            }
+        }
+    }
+
+    if dp[S] {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
