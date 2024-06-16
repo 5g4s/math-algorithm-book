01cf9b6050e620512168ab93217a0db72506a407
5g4s <kmvz9y6@gmail.com>
2024-06-16 22:09:20 +0900

    Fix Solution 8

--- src/bin/008.rs
+++ src/bin/008.rs
@@ -3,20 +3,18 @@
 use proconio::input;
 
 fn main() {
-    input!{
+    input! {
         N: u32,
         S: u32,
     }
-    
-    let mut answer:u32 = 0;
-    for red in 1..=N{
+
+    let mut answer: u32 = 0;
+    for red in 1..=N {
         for blue in 1..=N {
             if red + blue <= S {
-                answer+=1;
+                answer += 1;
             }
         }
     }
     println!("{}", answer);
 }
-
-
